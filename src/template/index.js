79d07449afafb9da4ea83a330673671137c2a2f6
zhuaoshuai13 <531024850@qq.com>
2022-12-29 17:33:14 +0800

    feat: change template

--- src/template/index.js
+++ src/template/index.js
@@ -14,20 +14,10 @@ $(() => {
   };
 
   const overview = {
-    sec1() {
-      const f1 = new TimelineMax().from('.sec1 .title', 0.7, { y: 200, opacity: 0 });
-
-      new ScrollMagic.Scene({
-        triggerElement: '.sec1',
-        triggerHook: util.isPc ? 0 : 0,
-        offset: -1000,
-      })
-        .setTween(f1)
-        .addTo(controller);
-    },
-
-    sec2() {
-      return 2;
+    test() {
+      return {
+        controller, util,
+      };
     },
 
     init() {
